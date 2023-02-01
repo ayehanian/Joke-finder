04d7d254858b8cdfe5f5084103e395880ad42345
Alina <52545883+ayehanian@users.noreply.github.com>
2023-02-01 16:29:53 +0200

    style main container to make pretty fullscreen

--- src/components/MainPage/styles.js
+++ src/components/MainPage/styles.js
@@ -1,6 +1,9 @@
 import {makeStyles} from "@material-ui/core/styles";
 
 const Styles = makeStyles((theme) => ({
+    container: {
+        background: "white",
+    },
     pageHeader: {
         display: "flex",
         justifyContent: "space-between",
